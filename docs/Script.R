2ab195211aa7627687d7a4543146893970d41a93
VictorWilmet <sv@sciviews.org>
2020-11-24 10:28:04 +0100

    oui

--- docs/Script.R
+++ docs/Script.R
@@ -6,10 +6,16 @@ biometry <- readr::read_csv("docs/biometrie_humaine_2019 - Feuille 1.csv", local
 biometry <- drop_na(biometry)
 
 biometry <- labelise(biometry, label=list(
-                                genre = "genre",
-                                taille = "taille",
-                                age = "age",
-                                masse = "masse",
-                                masse_perso = "masse personne",
-  ))
+  genre = "genre",
+  taille = "taille",
+  age = "age",
+  masse = "masse",
+  masse_perso = "masse personne",
+  masse_ref = "masse référence",
+  tour_hanche = "tour de hanche",
+  tour_taille = "tour de taille",
+  tour_poignet = "tour de poignet"
+  activite_phy = "tour"))
+
+
 
